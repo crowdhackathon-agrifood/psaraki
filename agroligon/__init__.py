c4d6f780091c73e68361028256999aa5ae2f8ada
Thanos Paravantis <thanosparavantis@gmail.com>
2019-04-05 20:23:03 +0300

    Initialize SQLAlchemy and DB location

--- agroligon/__init__.py
+++ agroligon/__init__.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
+    # Create Flask application
     app = Flask(__name__, instance_relative_config=True)
+
+    # Load configuration file from instance/ folder
     app.config.from_pyfile('config.py')
 
+    # Setup database SQLite3 file location
+    db_prefix = 'sqlite:///'
+    db_suffix = app.config['DB_FILE_NAME']
+    db_file_loc = f'{db_prefix}{app.instance_path}/{db_suffix}'
+
+    # Initialize SQLAlchemy ORM
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_file_loc
+    db.init_app(app)
+
+    # Register all app routes
     from agroligon import routes
     routes.init_routes(app)
 
