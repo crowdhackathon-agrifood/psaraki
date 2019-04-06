5dda73845af9e653aad26dacde4b06300a4d077a
Thanos Paravantis <thanosparavantis@gmail.com>
2019-04-06 14:19:12 +0300

    Second day

--- agroligon/routes/home.py
+++ agroligon/routes/home.py
@@ -2,6 +2,22 @@ from flask import Blueprint, render_template
 
 blueprint = Blueprint('home', __name__)
 
+
 @blueprint.route('/')
 def index():
-    return render_template('home/index.html')
+    return render_template('index.html')
+
+
+@blueprint.route('/search')
+def search():
+    return render_template('search.html')
+
+
+@blueprint.route('/producer')
+def producer():
+    return render_template('producer.html')
+
+
+@blueprint.route('/payment')
+def payment():
+    return render_template('payment.html')
